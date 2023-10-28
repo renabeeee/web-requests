42f1cbec6da44690ac89805469ff4e4c415e21a2
“Serena” <serenafgreen@gmail.com>
2023-10-28 13:09:27 -0700

    kanye west api quotes

---
+++ kanye_rest.rb
@@ -0,0 +1,39 @@
+puts "We're excited to welcome you to the SheHoopsLA team! Let's get started."
+
+puts "What's your first name?"
+first_name = gets.chomp
+
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts "What's the best email address to reach you?"
+email_address = gets.chomp
+
+puts "What's your phone number?"
+phone_number = gets.chomp
+
+require 'http'
+
+puts "Nice, thanks for telling us more about you. Can you confirm the details are correct? Please type in Yes or No"
+
+puts "Your first name: #{first_name}"
+puts "Your last name: #{last_name}"
+puts "Your email address: #{email_address}"
+puts "Your phone number: #{phone_number}"
+confirmation = gets.chomp
+
+confirmation1 = "Yes"
+confirmation2 = "No"
+
+if confirmation == "Yes"
+  puts "Great! While we get your account set up, please enjoy this quote by Kanye West."
+  response = HTTP.get('https://api.kanye.rest')
+
+data = response.parse
+
+pp data
+end
+
+if confirmation == "No"
+  puts "Okay! We'll give you access to your profile shortly to make changes."
+end
